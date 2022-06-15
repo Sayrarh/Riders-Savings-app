8992a938c80222a2f1bdafb590397ca8ad487ab9
Oluwatosin Serah <ajaotosinserah@gmail.com>
2022-06-15 16:53:57 +0100

    js update

--- script.js
+++ script.js
@@ -3,39 +3,25 @@ let amount = document.getElementById("amount");
 const form = document.getElementById("form");
 let tier = document.getElementById("tier");
 let interest;
+let percentage;
 
-//functions
-function showError(input, message) {
-    let formContent = input.parentElement
-    formContent.className = "form-content error"
-    let small = formContent.querySelector("small")
-    small.innerText = message
+
+function validate() {
+    let fullName = document.forms["myform"]["name"].value;
+    let selectTier = document.forms["myform"]["tier"].value;
+    let amountInput = document.forms["myform"]["amount"].value
+
+    //validations
+    if(fullName == "" || (!isNaN(fullName))){
+        alert("Enter your name");
+    }
+    if(selectTier == "Select preferred tier"){
+        alert("Select a tier!");
+    }
+    if(amountInput == "" ){
+        alert("Input an amount");
+    }
 }
 
-/*function showErrorSelect(select, message) {
-    let formContent = select.parentElement
-    formContent.className = "form-content error"
-    let small = formContent.querySelector("small")
-    small.innerText = message
-}*/
 
-function showSuccess(input){
-    let formContent = input.parentElement
-    formContent.className = "form-content success"
-}
 
-function checkRequired(inputArr){
-    inputArr.forEach(function(input){
-        if(input.value.trim() === ""){
-            showError(input, `${input.id} is required.` )
-        }else{
-            showSuccess(input)
-        }
-    })
-}
-
-//Event listener
-form.addEventListener("submit", function(event) {
-    event.preventDefault()
-    checkRequired([name, tier, amount])
-})
